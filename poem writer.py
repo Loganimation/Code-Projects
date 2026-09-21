70512d0519fde26192054daa6fce1585ee42afe2
Logan <theloganimation@gmail.com>
2026-09-21 11:54:43 -0400

    gahflallalahaba
    tinkered around with values and whatnot

--- poem writer.py
+++ poem writer.py
@@ -5,8 +5,7 @@ common = ["a","e","i","o","u"]
 options = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 p = random.choice(options.copy())
 q = random.choice(common.copy())
-space = 26
-rarity = 60
+space = 32
 for foob in range(random.randint(4,8)):
   p = random.choice(options.copy())
   q = random.choice(common.copy())
